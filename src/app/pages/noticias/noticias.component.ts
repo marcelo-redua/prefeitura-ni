31f41607e7b1df63101c323992cdf3fd81ed1729
unknown <user@DESKTOP-45UO36V.pmni>
2024-10-23 17:06:25 -0300

    eu e clovis renato

--- src/app/pages/noticias/noticias.component.ts
+++ src/app/pages/noticias/noticias.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HighlightTextComponent } from "../../components/highlight-text/highlight-text.component";
+import { NewsFeedComponent } from "../../components/news-feed/news-feed.component";
 
 @Component({
   selector: 'app-noticias',
   standalone: true,
-  imports: [HighlightTextComponent],
+  imports: [HighlightTextComponent, NewsFeedComponent],
   templateUrl: './noticias.component.html',
   styleUrl: './noticias.component.css'
 })
