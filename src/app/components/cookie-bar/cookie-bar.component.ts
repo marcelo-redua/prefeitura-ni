e19392f8e8533d288b6d29072af84aa3ab522177
unknown <user@DESKTOP-45UO36V.pmni>
2024-10-23 17:04:54 -0300

    tropa do cadu

--- src/app/components/cookie-bar/cookie-bar.component.ts
+++ src/app/components/cookie-bar/cookie-bar.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cookie-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cookie-bar.component.html',
-  styleUrl: './cookie-bar.component.css'
+  styleUrls: ['./cookie-bar.component.css']
 })
-export class CookieBarComponent {
+export class CookiePopupComponent {
+  cookiesAccepted: boolean = false;
 
+  constructor() {
+    this.checkCookieStatus();
+  }
+
+  checkCookieStatus(): void {
+    const cookieStatus = localStorage.getItem('cookieAccepted');
+    this.cookiesAccepted = cookieStatus === 'true';
+  }
+
+  acceptCookies(): void {
+    localStorage.setItem('cookieAccepted', 'true');
+    this.cookiesAccepted = true;
+    // Lógica para ativar cookies opcionais pode ser adicionada aqui
+  }
+
+  rejectCookies(): void {
+    localStorage.setItem('cookieAccepted', 'false');
+    this.cookiesAccepted = true;
+    // Lógica para desativar cookies opcionais
+  }
+
+  manageCookies(): void {
+    alert('Abrir gerenciador de cookies');
+    // Redirecionar para o gerenciador de cookies ou abrir um modal
+  }
 }
