29bdd27eb7412adeef496ecf433cc0a256fd711a
Marcelo Mesquita <marcelo.redua@outlook.com>
2024-11-05 12:07:27 -0300

    funciona pfvr

--- src/app/pages/semcult/semcult.component.ts
+++ src/app/pages/semcult/semcult.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HighlightTextComponent } from "../../components/highlight-text/highlight-text.component";
+import { SecretaryHeaderComponent } from "../../components/secretary-header/secretary-header.component";
+import { SecretaryQuickAccessComponent } from "../../components/secretary-quick-access/secretary-quick-access.component";
 
 @Component({
   selector: 'app-semcult',
   standalone: true,
-  imports: [],
+  imports: [HighlightTextComponent, SecretaryHeaderComponent, SecretaryQuickAccessComponent],
   templateUrl: './semcult.component.html',
   styleUrl: './semcult.component.css'
 })
