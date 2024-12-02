5a7aab5103d15f4f1ce862ff64c74cafad2c548a
Marcelo Mesquita <marcelo.redua@outlook.com>
2024-12-02 14:05:20 -0300

    telefones

--- src/app/components/phone-list/phone-list.component.ts
+++ src/app/components/phone-list/phone-list.component.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-phone-list',
-  imports: [RouterModule, CommonModule], // Importação do RouterModule
+  imports: [RouterModule, CommonModule],
   templateUrl: './phone-list.component.html',
   styleUrls: ['./phone-list.component.css'],
   standalone: true
@@ -15,81 +15,118 @@ export class PhoneListComponent {
       title: 'Defesa Civil',
       description: 'para emergências relacionadas à defesa civil.',
       iconSrc: '/assets/icons/defesa-civil.svg',
-      phones: ['199', '2667-5751', '98160-9740']
+      phones: [
+        { label: 'Central:', number: '199' },
+        { label: '', number: '2667-5751' },
+        { label: '', number: '98160-9740' }
+      ]
     },
     {
       title: 'Polícia Militar',
       description: 'para emergências de segurança pública.',
       iconSrc: '/assets/icons/policia-militar.svg',
-      phones: ['190']
+      phones: [
+        { label: 'Central:', number: '190' }
+      ]
     },
     {
       title: 'Bombeiros',
       description: 'para incêndios e resgates.',
       iconSrc: '/assets/icons/bombeiros.svg',
-      phones: ['193']
+      phones: [
+        { label: 'Central:', number: '193' }
+      ]
     },
     {
       title: '4º Grupamento de Bombeiro Militar (Moquetá)',
       description: 'para incêndios e resgates.',
       iconSrc: '/assets/icons/bombeiros.svg',
-      phones: ['2669-2592','2669-2093','2669-2789','2669-3775']
+      phones: [
+        { label: '', number: '2669-2592' },
+        { label: '', number: '2669-2093' },
+        { label: '', number: '2669-2789' },
+        { label: '', number: '2669-3775' }
+      ]
     },
     {
       title: '52º DP',
-      description: 'para incêndios e resgates.',
+      description: 'para emergências de segurança pública.',
       iconSrc: '/assets/icons/policia-militar.svg',
-      phones: ['3779-0834','3779-0168']
+      phones: [
+        { label: '', number: '3779-0834' },
+        { label: '', number: '3779-0168' }
+      ]
     },
     {
       title: '56º DP',
-      description: 'para incêndios e resgates.',
+      description: 'para emergências de segurança pública.',
       iconSrc: '/assets/icons/policia-militar.svg',
-      phones: ['2669-4748',' 2669-0221']
+      phones: [
+        { label: '', number: '2669-4748' },
+        { label: '', number: '2669-0221' }
+      ]
     },
     {
       title: '58º DP',
-      description: 'para incêndios e resgates.',
+      description: 'para emergências de segurança pública.',
       iconSrc: '/assets/icons/policia-militar.svg',
-      phones: ['3779-9540']
+      phones: [
+        { label: '', number: '3779-9540' }
+      ]
     },
     {
       title: 'Delegacia Especializada de Atendimento à Mulher',
-      description: 'para incêndios e resgates.',
+      description: 'para atendimento às mulheres.',
       iconSrc: '/assets/icons/mulheres.svg',
-      phones: ['180',' 3779-9555']
+      phones: [
+        { label: 'Central:', number: '180' },
+        { label: '', number: '3779-9555' }
+      ]
     },
     {
       title: 'Hospital Geral de Nova Iguaçu',
-      description: 'para incêndios e resgates.',
+      description: 'para emergências hospitalares.',
       iconSrc: '/assets/icons/hospital.svg',
-      phones: ['3779-0308']
+      phones: [
+        { label: '', number: '3779-0308' }
+      ]
     },
     {
       title: 'Central de Regulação de Exames',
-      description: 'para incêndios e resgates.',
+      description: 'para agendamento de exames.',
       iconSrc: '/assets/icons/central-exames.svg',
-      phones: ['3510-5210']
+      phones: [
+        { label: '', number: '3510-5210' }
+      ]
     },
     {
       title: 'Clínica Dom Walmor',
-      description: 'para incêndios e resgates.',
+      description: 'atendimento clínico.',
       iconSrc: '/assets/icons/clinica.svg',
-      phones: ['2767-9812']
+      phones: [
+        { label: '', number: '2767-9812' }
+      ]
     },
     {
       title: 'Ouvidoria da Prefeitura',
-      description: 'para incêndios e resgates.',
+      description: 'para atendimento à população.',
       iconSrc: '/assets/icons/ouvidoria.svg',
-      phones: ['2666-4910']
+      phones: [
+        { label: '', number: '2666-4910' }
+      ]
     },
     {
       title: 'Conselho Tutelar',
-      description: 'para incêndios e resgates.',
+      description: 'para atendimento de casos envolvendo menores.',
       iconSrc: '/assets/icons/conselho.svg',
-      phones: ['2668-5568','2657-4510','2763-2484','3769-6487','3766-0308']
-    },
-    // Adicione os demais itens aqui.
+      phones: [
+        { label: 'Centro: 2668-5568', number: '2668-5568' },
+        { label: 'Cabuçu: 2657-4510', number: '2657-4510' },
+        { label: 'Austin: 2763-2484', number: '2763-2484' },
+        { label: 'Vila de Cava:', number: '3769-6487' },
+        { label: 'Comendador Soares:', number: '3766-0308' }
+      ]
+    }
   ];
 
   generatePhoneHref(phone: string): string {
